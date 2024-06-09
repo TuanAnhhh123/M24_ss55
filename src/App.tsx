7c9626c88464ae0816b525d76f7e06598f8562f5
TuanAnhhh123 <nguyentuananh11a4@gmail.com>
2024-06-09 18:55:26 +0700

    add

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>App
+      {/* 
+        API : application programing interface
+        giúp cho các ứng dụng có thể giao tiếp vs database , tương tác vs cơ cơ dở dữ liệu
+      */}
+    </div>
+  )
+}
